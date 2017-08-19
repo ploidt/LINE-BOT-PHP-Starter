1e590ca48a5db5225c2f6691656a955b8a87410f
ploidt <ploid@Ploid-Macbook.local>
2017-08-19 21:20:04 +0700

    test

---
+++ bot.php
@@ -0,0 +1,23 @@
+<?php
+
+include ('./php/line-bot.php');
+
+$access_token = 'OS3ckApBsExdHU+lPbwqpN9joiqCF6Xkir7G8+3Odh3yoPU+b3eoZDmu0Yon7wd3xvEg8yA1Q+bBQruqlGC/iy+eUXV+ViUQ76dytwE7pLh8cOvvWj0Et+8bXNSDae/QcyFkeu6tqj6xdQFrtFndSAdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'dc4aa780d6557e4dc579fc51661eccbd';
+
+$bot = new BOT_API($channelSecret, $access_token);
+    
+if (!empty($bot->isEvents)) {
+        
+    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+
+    if ($bot->isSuccess()) {
+        echo 'Succeeded!';
+        exit();
+    }
+
+    // Failed
+    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+    exit();
+
+}
