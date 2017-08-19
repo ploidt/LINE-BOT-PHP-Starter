f267b02bfe84e293348a16d9a1ba77ffcb93c11e
ploidt <ploid@Ploid-Macbook.local>
2017-08-19 21:22:52 +0700

    test

--- bot.php
+++ bot.php
@@ -1,23 +1,24 @@
 <?php
 
+echo 'test';
 include ('php/line-bot.php');
 
-$access_token = 'OS3ckApBsExdHU+lPbwqpN9joiqCF6Xkir7G8+3Odh3yoPU+b3eoZDmu0Yon7wd3xvEg8yA1Q+bBQruqlGC/iy+eUXV+ViUQ76dytwE7pLh8cOvvWj0Et+8bXNSDae/QcyFkeu6tqj6xdQFrtFndSAdB04t89/1O/w1cDnyilFU=';
-$channelSecret = 'dc4aa780d6557e4dc579fc51661eccbd';
+// $access_token = 'OS3ckApBsExdHU+lPbwqpN9joiqCF6Xkir7G8+3Odh3yoPU+b3eoZDmu0Yon7wd3xvEg8yA1Q+bBQruqlGC/iy+eUXV+ViUQ76dytwE7pLh8cOvvWj0Et+8bXNSDae/QcyFkeu6tqj6xdQFrtFndSAdB04t89/1O/w1cDnyilFU=';
+// $channelSecret = 'dc4aa780d6557e4dc579fc51661eccbd';
 
-$bot = new BOT_API($channelSecret, $access_token);
+// $bot = new BOT_API($channelSecret, $access_token);
     
-if (!empty($bot->isEvents)) {
+// if (!empty($bot->isEvents)) {
         
-    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+//     $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
 
-    if ($bot->isSuccess()) {
-        echo 'Succeeded!';
-        exit();
-    }
+//     if ($bot->isSuccess()) {
+//         echo 'Succeeded!';
+//         exit();
+//     }
 
-    // Failed
-    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
-    exit();
+//     // Failed
+//     echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+//     exit();
 
-}
+// }
