6fa2c6d66efb7c3dfff22c2ea4bc7834d670075c
ploidt <ploid@Ploid-Macbook.local>
2017-08-19 23:31:55 +0700

    test menu

--- bot.php
+++ bot.php
@@ -3,6 +3,10 @@ require __DIR__."/vendor/autoload.php";
 
 $access_token = 'OS3ckApBsExdHU+lPbwqpN9joiqCF6Xkir7G8+3Odh3yoPU+b3eoZDmu0Yon7wd3xvEg8yA1Q+bBQruqlGC/iy+eUXV+ViUQ76dytwE7pLh8cOvvWj0Et+8bXNSDae/QcyFkeu6tqj6xdQFrtFndSAdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'dc4aa780d6557e4dc579fc51661eccbd';
+$app_id = '9d511b82';
+$app_key = 'dfcce54f444d706e750cffb9e523c9d1';
+
+$url =  "https://api.edamam.com/search?app_id=9d511b82&app_key=dfcce54f444d706e750cffb9e523c9d1&from=0&to=3&q=";
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
@@ -22,8 +26,33 @@ if (!is_null($events['events'])) {
             // Get replyToken
             $replyToken = $event['replyToken'];
 
-            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
-            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+            $text_ex = explode(':', $text); //เอาข้อความมาแยก : ได้เป็น Array
+
+            if($text_ex[0] === "menu"){
+                $ch1 = curl_init();
+                curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch1, CURLOPT_URL, $url.$text_ex[1]);
+                $result1 = curl_exec($ch1);
+                curl_close($ch1);
+                
+                $obj = json_decode($result1, true);
+                
+                foreach($obj['hits']['recipe'] as $key => $val){
+
+                    $result_text = $val['label'];
+                    echo $result_text;
+                }
+
+                if(empty($result_text)){//ไม่พบข้อมูล ตอบกลับไป
+                    $result_text = 'ไม่พบข้อมูล';
+                }
+
+            }else{
+                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($result_text);
+                $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+            }
+
         }
     }
 }
---
+++ recipe.php
@@ -0,0 +1,6 @@
+<?php 
+
+$app_id = '9d511b82';
+$app_key = 'dfcce54f444d706e750cffb9e523c9d1';
+
+curl "https://api.edamam.com/search?q=chicken&app_id=9d511b82&app_key=dfcce54f444d706e750cffb9e523c9d1&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free";
